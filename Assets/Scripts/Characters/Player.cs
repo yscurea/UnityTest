39c9d5195db0c4390a99076bc0bd922f641714f0
yuusuine <yuuki.990510@gmail.com>
2020-03-01 13:30:54 +0900

    first commit

---
+++ Assets/Scripts/Characters/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	//[SerializeField]
+	//private Status status;
+
+	[SerializeField]
+	private GameObject gun;
+	[SerializeField]
+	private Weapon.GunController gunController;
+
+	private int shootSpan = 10;
+	private int spanTime = 10;
+
+	void Start()
+	{
+		gunController = gun.GetComponent<Weapon.GunController>();
+	}
+
+	void Update()
+	{
+		Action();
+	}
+
+	private void FixedUpdate()
+	{
+		
+	}
+
+	private void Action()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			shootSpan--;
+			if (shootSpan == 0)
+			{
+				shootSpan = spanTime;
+				gunController.Shoot();
+			}
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/Weapons/GunController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Weapon
+{
+	public class GunController : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject bullet;
+
+		private float bulletSpeed = 20.0f;
+
+		private int bulletNum = 100;
+
+		private float positionMargin = 1.0f;
+
+
+
+		public void Shoot()
+		{
+			if (bulletNum > 0)
+			{
+				Vector3 startPosition = this.transform.position;
+
+				startPosition += this.transform.forward * positionMargin;
+
+				GameObject bulletTmp = Instantiate(bullet, startPosition, this.transform.rotation);
+
+				bulletTmp.GetComponent<Rigidbody>().velocity = this.transform.forward * bulletSpeed;
+
+				bulletNum--;
+			}
+			else
+			{
+				//弾切れ
+			}
+		}
+	}
+}
